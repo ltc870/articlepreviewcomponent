679d52b395b13dae527983d7aaa848d0336c3f2e
Lawrence C <lawrencecaudle870@gmail.com>
2022-09-12 22:26:34 -0400

    added functionality to buttons in index.js

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,17 @@ const shareBtn = document.getElementById("share");
 console.log(shareBtn);
 const footer = document.querySelector("#footer");
 console.log(footer);
+const socialMedia = document.querySelector("#social-media");
+console.log(socialMedia);
+const socialMediaShare = document.querySelector("#social-media-share");
+console.log(socialMediaShare);
 
 shareBtn.addEventListener("click", () => {
-    footer.classList.toggle("show");
+    footer.classList.add("hide");
+    socialMedia.classList.add("show");
+});
+
+socialMediaShare.addEventListener("click", () => {
+    footer.classList.remove("hide");
+    socialMedia.classList.remove("show");
 });
