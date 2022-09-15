592b8b8ad74595764154633f5cb69d67d31b09d0
Lawrence C <lawrencecaudle870@gmail.com>
2022-09-15 15:02:50 -0400

    removed unneeded code

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,20 @@
 "use strict";
 
 const shareBtn = document.getElementById("share");
-console.log(shareBtn);
 const footer = document.querySelector("#footer");
-console.log(footer);
 const socialMedia = document.querySelector("#social-media");
-console.log(socialMedia);
 
 shareBtn.addEventListener("click", (event) => {
     socialMedia.classList.toggle("show");
     event.currentTarget.classList.toggle("active");
+});
+
+document.addEventListener("click", (event) => {
+    if (
+        !socialMedia.contains(event.target) &&
+        !shareBtn.contains(event.target)
+    ) {
+        shareBtn.classList.remove("active");
+        socialMedia.classList.remove("show");
+    }
 });
