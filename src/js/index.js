ee263e3fd88208dda1d752d80023bbffd2ea59b3
Lawrence C <lawrencecaudle870@gmail.com>
2022-09-15 12:52:53 -0400

    removed unused code

--- src/js/index.js
+++ src/js/index.js
@@ -6,8 +6,6 @@ const footer = document.querySelector("#footer");
 console.log(footer);
 const socialMedia = document.querySelector("#social-media");
 console.log(socialMedia);
-const socialMediaShare = document.querySelector("#social-media-share");
-console.log(socialMediaShare);
 
 shareBtn.addEventListener("click", (event) => {
     socialMedia.classList.toggle("show");
